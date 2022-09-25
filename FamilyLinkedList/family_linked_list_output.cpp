513927add81d636738195a8f78c2ac85eee94823
AkshitSharmaCoder <102974166+AkshitSharmaCoder@users.noreply.github.com>
2022-09-25 12:23:23 +0530

    Create family_linked_list_output.cpp

---
+++ FamilyLinkedList/family_linked_list_output.cpp
@@ -0,0 +1,28 @@
+(base) akshitsharma@Akshits-MacBook-Air DSApyCLLG % cd "/Users/akshitsharma/Desktop/DSApyCLLG/" && g++ FamilyLinkedList_answer.cpp -o FamilyLinkedList_an
+swer && "/Users/akshitsharma/Desktop/DSApyCLLG/"FamilyLinkedList_answer
+
+
+Enter Number Of Family Members:4
+Enter Family Member 1 Name:
+akshit
+Enter Family Member 1 Age:
+19
+Enter Family Member 2 Name:
+tamanna
+Enter Family Member 2 Age:
+23
+Enter Family Member 3 Name:
+ashwani
+Enter Family Member 3 Age:
+40
+Enter Family Member 4 Name:
+sapna
+Enter Family Member 4 Age:
+49
+
+Doubly Linked list is : 
+
+[Name:akshit Age:19]<=>[Name:tamanna Age:23]<=>[Name:ashwani Age:40]<=>[Name:sapna Age:49]%            
+  
+  
+(base) akshitsharma@Akshits-MacBook-Air DSApyCLLG % 
